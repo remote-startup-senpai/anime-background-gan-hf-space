51a7dff4c9fff93d5a347f0834fbedaeb7c08dd5
秋山翔 <35907066+akiyamasho@users.noreply.github.com>
2022-01-08 03:31:56 +0900

    TEST: debug gradio not rendering

--- app.py
+++ app.py
@@ -16,36 +16,38 @@ STYLE = "shinkai_makoto"
 MODEL_PATH = "models"
 COLOUR_MODEL = "RGB"
 
-model = Transformer()
-model.load_state_dict(torch.load(os.path.join(MODEL_PATH, f"{STYLE}.pth")))
-model.eval()
+# model = Transformer()
+# model.load_state_dict(torch.load(os.path.join(MODEL_PATH, f"{STYLE}.pth")))
+# model.eval()
 
-disable_gpu = torch.cuda.is_available()
+# disable_gpu = torch.cuda.is_available()
 
 
 def inference(img):
-    # load image
-    input_image = img.convert(COLOUR_MODEL)
-    input_image = np.asarray(input_image)
-    # RGB -> BGR
-    input_image = input_image[:, :, [2, 1, 0]]
-    input_image = transforms.ToTensor()(input_image).unsqueeze(0)
-    # preprocess, (-1, 1)
-    input_image = -1 + 2 * input_image
+    # # load image
+    # input_image = img.convert(COLOUR_MODEL)
+    # input_image = np.asarray(input_image)
+    # # RGB -> BGR
+    # input_image = input_image[:, :, [2, 1, 0]]
+    # input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+    # # preprocess, (-1, 1)
+    # input_image = -1 + 2 * input_image
 
-    if disable_gpu:
-        input_image = Variable(input_image).float()
-    else:
-        input_image = Variable(input_image).cuda()
+    # if disable_gpu:
+    #     input_image = Variable(input_image).float()
+    # else:
+    #     input_image = Variable(input_image).cuda()
 
-    # forward
-    output_image = model(input_image)
-    output_image = output_image[0]
-    # BGR -> RGB
-    output_image = output_image[[2, 1, 0], :, :]
-    output_image = output_image.data.cpu().float() * 0.5 + 0.5
+    # # forward
+    # output_image = model(input_image)
+    # output_image = output_image[0]
+    # # BGR -> RGB
+    # output_image = output_image[[2, 1, 0], :, :]
+    # output_image = output_image.data.cpu().float() * 0.5 + 0.5
 
-    return output_image
+    # return output_image
+
+    return ""
 
 
 title = "AnimeBackgroundGAN"
